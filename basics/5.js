88541113820669ecb2e5d83e81b1576fb16eb78c
1975udit <113414526+1975udit@users.noreply.github.com>
2024-02-01 09:49:07 +0000

    memory

--- basics/5.js
+++ basics/5.js
@@ -8,10 +8,31 @@ const id2 = Symbol("123");
 console.log(id1 == id2); // it return False
 
 // Referenced (Non-Primitive) -> Array, Object, Function.
- const Heros = [Shaktimaan, Krish, Naagraj];                     //ARRAY
+//  const Heros = [Shaktimaan, Krish, Naagraj];                     //ARRAY
 
  let obj = { name:"Udit", age:21}                                //OBJECT
 
  const fun =  function() {                                                    //FUNCTION
     console.log("Hello");
  }
+
+
+//  Types of memory in js 
+// 1 -> heap (non-premitive data types uses it) --                       copy is provided
+// 2 -> stack (premitive data type uses it) --                        reference is provided (address)
+let a =12;
+let b =a;
+a =15;
+console.table([a,b]);
+// no change in value of a bcz copy is provided to b
+
+let user1 ={
+   name : "xyz",
+   age : 24
+}
+
+let user2 = user1;
+user2.age = 50;
+console.log(user1.age);
+console.log(user2.age);
+// both values will be changed bcz addres is pronided to user2 
