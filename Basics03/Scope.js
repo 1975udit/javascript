289f990b97dc23055e034bb8083dbfdac307d4cc
1975udit <113414526+1975udit@users.noreply.github.com>
2024-02-25 04:47:03 +0000

    new

--- Basics03/Scope.js
+++ Basics03/Scope.js
@@ -1,15 +1,16 @@
 // S C O P E     with    F U N C T I O N S
 
-// function one(){
-//     const user = "udit";
-//     console.log(user);
+function one(){
+    const user = "udit";
+    console.log(user);
 
-//     function two(){
-//         console.log(user);
-//     }
-//     // console.log(web);
-//     two();
-// }
-// one();
+    function two(){
+        const web = "YT"
+        console.log(user);
+    }
+    // console.log(web); This is out of scope 
+    two();
+}
+one();
 
 console.log("hello")
