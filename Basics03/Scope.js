adc4c440ef851ce9634d379d12b31b7a7fdf6aaa
1975udit <113414526+1975udit@users.noreply.github.com>
2024-02-25 04:30:17 +0000

    new

---
+++ Basics03/Scope.js
@@ -0,0 +1,15 @@
+// S C O P E     with    F U N C T I O N S
+
+// function one(){
+//     const user = "udit";
+//     console.log(user);
+
+//     function two(){
+//         console.log(user);
+//     }
+//     // console.log(web);
+//     two();
+// }
+// one();
+
+console.log("hello")
