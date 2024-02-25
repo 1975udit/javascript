8772fb685a1058e1bc49b815097a0a7581c605c8
1975udit <113414526+1975udit@users.noreply.github.com>
2024-02-25 13:48:23 +0000

    new

--- Basics03/Scope.js
+++ Basics03/Scope.js
@@ -12,5 +12,14 @@ function one(){
     two();
 }
 one();
+// same concept for the if else statement
 
 console.log("hello")
+
+// variable form of writing a function
+const addOne = function(value){
+    return value+1;
+}
+console.log(addOne(3));
+// But in this form of function decleration we can't call the function before its defination but in other type we can do that the reason behind this is we are holding this function in a variable
+
