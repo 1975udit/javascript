4b1a289cd2abca8c6747efab21feab7e4d53fe3f
1975udit <113414526+1975udit@users.noreply.github.com>
2024-03-26 04:02:28 +0000

    del

--- advance_01/fetch.js
+++
@@ -1,9 +0,0 @@
-fetch("https://api.github.com/users/hiteshchoudhary")
-.then(function (response){
-    return response.json()
-}).then(function (data){
-    console.log(data);
-}).catch(function(error){
-    console.log(error)
-}) 
-// continue
